8533d24c0eb905ae319dfb48def56e47dd8648f6
Khandker Mahmudur Rahman <mahmudur.rahman@fieldnation.com>
2026-05-14 18:15:28 +0600

    feat: add Android emulator to PATH for improved development setup

--- home/shell.nix
+++ home/shell.nix
@@ -74,6 +74,7 @@
       if [ -d "$ANDROID_HOME" ]; then
         
         export PATH="$ANDROID_HOME/cmdline-tools/latest/bin:$PATH"
+        export PATH="$ANDROID_HOME/emulator:$PATH"
         export PATH="$ANDROID_HOME/platform-tools:$PATH"
         export PATH="$ANDROID_HOME/tools:$PATH"
         export PATH="$ANDROID_HOME/tools/bin:$PATH"
